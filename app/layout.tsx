98ca25e3786b31d65a5f9c93882c892d11c378de
Denis <callium@gmail.com>
2026-08-06 23:07:26 -0300

    fix: add no-referrer policy for invitation links

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
+      <head>
+        <meta name="referrer" content="no-referrer" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
